60a149e67eb68a3a936382943b31bcefdf79c150
再度 <zaidu9527@gmail.com>
2025-07-31 14:37:56 +0000

    在两个下载按钮前面增加一个谷歌商店下载按钮，按钮背景用https://cdn.apks.cc/blinko/1753972567787-1753

--- src/components/GameDownloadButtons.tsx
+++ src/components/GameDownloadButtons.tsx
@@ -14,6 +14,11 @@ import Link from 'next/link';
 export function GameDownloadButtons() {
     return (
         <div className="flex flex-col sm:flex-row gap-4">
+            <Button asChild size="lg" className="animated-border-btn !p-0 w-[200px] h-[58px] bg-cover bg-center hover:scale-105 transition-transform duration-300 border border-white/50" style={{backgroundImage: "url('https://cdn.apks.cc/blinko/1753972567787-1753972566208-google_apk_download.png')"}}>
+                <Link href="https://play.google.com/store/apps/details?id=com.tencent.ig" target="_blank">
+                    <span className="sr-only">Download on Google Play</span>
+                </Link>
+            </Button>
             <Button asChild size="lg" className="animated-border-btn !p-0 w-[200px] h-[58px] bg-cover bg-center hover:scale-105 transition-transform duration-300 border border-white/50" style={{backgroundImage: "url('https://cdn.apks.cc/blinko/1753972022261-1753972021905-app_store.png')"}}>
                 <Link href="#">
                     <span className="sr-only">Download on the App Store</span>
