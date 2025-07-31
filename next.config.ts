b98ced10f1d88194f28306951e35758895782cac
再度 <zaidu9527@gmail.com>
2025-07-31 14:25:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.apks.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
