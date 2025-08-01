753a0d733cd85df479e119c246f7336cbb5fdd6b
再度 <zaidu9527@gmail.com>
2025-08-01 17:46:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <code>
    
    with the t

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
 
 interface MarkdownContentProps {
   content: string;
@@ -8,7 +9,7 @@ interface MarkdownContentProps {
 export function MarkdownContent({ content }: MarkdownContentProps) {
   return (
     <div className="prose prose-invert prose-lg max-w-none text-foreground/80">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm, remarkBreaks]}>{content}</ReactMarkdown>
     </div>
   );
 }
