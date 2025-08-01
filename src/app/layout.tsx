b74071b460a3e50bd60d564a081c6de666b82ee5
再度 <zaidu9527@gmail.com>
2025-08-01 05:54:17 +0000

    调整layout.tsx 代码结构，提升健壮性，避免出现<Html> 错误使用问题

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
       <body className="font-body antialiased bg-background text-foreground">
         <div className="flex flex-col min-h-screen">
           <Header />
