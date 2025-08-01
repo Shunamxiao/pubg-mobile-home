f6b2c9f762bed6b154e347008a0829456929c20d
再度 <zaidu9527@gmail.com>
2025-08-01 17:25:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/GameDownloadButtons.tsx
+++ src/components/GameDownloadButtons.tsx
@@ -15,12 +15,12 @@ import { siteConfig } from '@/config/site';
 export function GameDownloadButtons() {
     return (
         <div className="flex flex-col sm:flex-row gap-4">
-            <Button asChild size="lg" className="animated-border-btn !p-0 w-[180px] h-[52px] sm:w-[200px] sm:h-[58px] bg-cover bg-center hover:scale-105 transition-transform duration-300 border border-white/50">
+            <Button asChild size="lg" className="animated-border-btn !p-0 w-[180px] h-[52px] sm:w-[200px] sm:h-[58px] bg-cover bg-center hover:scale-105 transition-transform duration-300 border border-white/50" style={{backgroundImage: `url('${siteConfig.downloads.googlePlay.backgroundImage}')`}}>
                 <Link href={siteConfig.downloads.googlePlay.url} target="_blank">
                     <span className="sr-only">{siteConfig.downloads.googlePlay.srText}</span>
                 </Link>
             </Button>
-            <Button asChild size="lg" className="animated-border-btn !p-0 w-[180px] h-[52px] sm:w-[200px] sm:h-[58px] bg-cover bg-center hover:scale-105 transition-transform duration-300 border border-white/50">
+            <Button asChild size="lg" className="animated-border-btn !p-0 w-[180px] h-[52px] sm:w-[200px] sm:h-[58px] bg-cover bg-center hover:scale-105 transition-transform duration-300 border border-white/50" style={{backgroundImage: `url('${siteConfig.downloads.appStore.backgroundImage}')`}}>
                 <Link href={siteConfig.downloads.appStore.url}>
                     <span className="sr-only">{siteConfig.downloads.appStore.srText}</span>
                 </Link>
