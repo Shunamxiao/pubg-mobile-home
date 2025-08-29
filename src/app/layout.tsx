d4248180a7bdc0da67f23291948167cc43d42e1f
再度 <zaidu9527@gmail.com>
2025-08-29 11:52:50 +0000

    修改下逻辑，将配置文件 (src/config/site.ts)配置项analytics字段的内容，修改为在页面未渲染完成时就已经添加到head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Toaster } from "@/components/ui/toaster";
 import { siteConfig } from '@/config/site';
-import { AnalyticsScript } from '@/components/AnalyticsScript';
-import { CustomHead } from '@/components/CustomHead';
 
 // Metadata is still useful for static tags
 export const metadata: Metadata = {
@@ -30,6 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-Hans" className="dark">
+      <head>
+        {siteConfig.analytics.customHeadHtml && (
+          <div dangerouslySetInnerHTML={{ __html: siteConfig.analytics.customHeadHtml }} />
+        )}
+      </head>
       <body className="font-body antialiased bg-background text-foreground">
         <div className="flex flex-col min-h-screen">
           <Header />
@@ -37,11 +40,10 @@ export default function RootLayout({
           <Footer />
         </div>
         <Toaster />
-        <AnalyticsScript />
-        <CustomHead />
+        {siteConfig.analytics.customBodyScript && (
+          <script dangerouslySetInnerHTML={{ __html: siteConfig.analytics.customBodyScript }} />
+        )}
       </body>
     </html>
   );
 }
-
-    
